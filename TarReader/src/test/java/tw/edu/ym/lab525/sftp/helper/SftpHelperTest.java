4ac239162478518a9b7779b6fb46740c551b23a1
Ming-Jheng Li <mjli@ym.edu.tw>
2014-09-16 20:57:16 +0800

    Add author

--- TarReader/src/test/java/tw/edu/ym/lab525/sftp/helper/SftpHelperTest.java
+++ TarReader/src/test/java/tw/edu/ym/lab525/sftp/helper/SftpHelperTest.java
@@ -1,5 +1,8 @@
 package tw.edu.ym.lab525.sftp.helper;
 
+/**
+ * @author Minh-Jheng Li
+ */
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
